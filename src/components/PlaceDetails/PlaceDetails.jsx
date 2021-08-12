b64bf84d09804567d3afffc01f2a6bae0d61410c
Harvindsokhal <harvindsokhal@gmail.com>
2021-08-12 21:50:17 +0100

    Travel Advisor and website links

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -49,7 +49,14 @@ const PlaceDetails = ({ place }) => {
                     </Typography>
                 )}
 
-
+                <CardActions>
+                    <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+                        Trip Advisor
+                    </Button>
+                    <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+                        Website
+                    </Button>
+                </CardActions>
             </CardContent>
         </Card>
     )
