12279e6770ea3fb6c3e0466fffe21b16f85158c8
Harvindsokhal <harvindsokhal@gmail.com>
2021-08-13 22:22:52 +0100

    Adding Pins to map

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ const App = () => {
                         setCoordinates={setCoordinates}
                         setBounds={setBounds}
                         coordinates={coordinates}
+                        places={places}
                     />
                 </Grid>
             </Grid>
